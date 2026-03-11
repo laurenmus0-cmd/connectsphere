0d3d780b1df353ceaf48b29ed86c4134c9803279
rodgers-ctrl <rodgerskiria25@gmail.com>
2026-03-11 14:43:21 +0300

    users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from .models import Profile
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+#homepage
+def home(request):
+    return render(request, 'users/home.html')
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # create profile for additional fields
+            Profile.objects.create(user=user, location=request.POST.get('location', ''))
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+#login and logout views
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('register')  # or your homepage
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'users/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login') 
+        
